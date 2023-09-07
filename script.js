628318f6c4b61c8ed3ece7e97e801a1a005baf6f
rlrx <ryan.lee.ruixiang@gmail.com>
2023-09-07 23:43:55 +0800

    Implemented eventListener for mouse hover

--- script.js
+++ script.js
@@ -4,7 +4,13 @@ let i = 16*16;
 while(i>0){
     let grid = document.createElement("div");
     grid.classList.add('grid');
+    // add eventListener to the grid box
+    grid.addEventListener('mouseover', () => {
+        grid.style.backgroundColor = "pink";
+    });
+    // adding the element into the DOM
     gridContainer.appendChild(grid);
     console.log(`grid ${i} created`);
     i--;
 };
+
